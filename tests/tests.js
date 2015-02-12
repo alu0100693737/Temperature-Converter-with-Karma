2ad30f2a00e328c5fc4519424d5a5b6837bd5f5c
Ivan Garcia <alu0100693737@ull.edu.es>
2015-02-12 17:21:01 +0000

    Tests hechos

---
+++ tests/tests.js
@@ -0,0 +1,35 @@
+var assert = chai.assert;
+
+suite('temperature', function() {
+    test('32F = 0C', function() {
+        original.value = "32F";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+    });
+    test('12F = -11.1C', function() {
+        original.value = "12F";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "-11.1 Celsius");
+    });
+    test('6.1C = 43F', function() {
+        original.value = "6.1C";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "43.0 Farenheit");
+    });
+    test('3te = Numero introducido no valido', function() {
+        original.value = "3te";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "ERROR! Numero introducido no valido, observe los ejemplos");
+    });
+    
+    test('45C = 113.0 Farenheit', function() {
+        original.value = "45C";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+    });
+    test('5X = error', function() {
+        original.value = "5X";
+        calculate();
+        assert.match(converted.innerHTML, /ERROR/);
+    });
+});
