9ac91c6282398127a14685b2e380b285b6c5ace5
Ivan Garcia <alu0100693737@ull.edu.es>
2015-02-21 23:36:55 +0000

    Mas modificaciones

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,10 @@ var gulp    = require('gulp'),
     uglify  = require('gulp-uglify'),
     concat  = require('gulp-concat');
 var karma   = require('gulp-karma');
-...
+var del     = require('del');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
 
-gulp.task('test', function() {
-  // Be sure to return the stream
-  return gulp.src([])
-    .pipe(karma({
-      configFile: 'karma.conf.js',
-      action: 'run'
-    }))
-    .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
-});
 
 gulp.task('default', function() {
   gulp.src([])
@@ -27,3 +17,22 @@ gulp.task('default', function() {
     }));
 });
 
+
+
+gulp.task('test', function () {
+  gulp.src('temperature.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./*.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/'))
+});
+
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
+});
